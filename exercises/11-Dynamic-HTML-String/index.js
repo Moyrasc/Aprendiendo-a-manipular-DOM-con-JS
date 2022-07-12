877eac75f1fe190ec49fd5a9110a345741df5ba2
Moyra_sc <noemisolac@gmail.com>
2022-07-12 13:35:25 +0000

    ejercicios 11 y 12 resueltos

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,8 +1,17 @@
 let myString = `<p>Hello!</p> <strong>My friend</strong>`;
-document.write(myString);
+document.write(myString)
+
+
+function year() {
+     let day = new Date(); 
+     let now = day.getFullYear(); 
+     document.getElementsByName("myString").innerHTML = now; 
+}
+document.write(myString.append(year));
 
 // newDate = new Date()
 // let year = newDate.getFullYear()
 // console.log(year)
 // let yearString = `we are in the year`
 // myString.append(`we are in the year`, year)
+
--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,18 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+let selectCountries = document.querySelector("#mySelect");
+countries.forEach(country => {
+    let optionCountry = document.createElement("option")
+    optionCountry.innerHTML = country;
+    selectCountries.appendChild(optionCountry)
+})
+
+
+selectCountries.addEventListener("change", (e) => {
+    const select = `${e.target.value}`
+    alert(select)
+})
+
+
+
