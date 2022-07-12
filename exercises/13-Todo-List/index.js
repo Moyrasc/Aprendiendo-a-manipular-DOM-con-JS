f71a169f570c5c51bdb2641e928843db666e43f8
Moyra_sc <noemisolac@gmail.com>
2022-07-12 17:58:36 +0000

    ejercicios finalizados

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,19 +1,36 @@
 // Your code here
-const tareaNueva = document.querySelector("#addToDo")
-const borrarTarea = document.querySelector(".fa fa-trash")
+const newTask = document.querySelector("#addToDo");
+// const deleteTask = document.querySelector(".fa-trash");
 
+newTask.addEventListener("keyup", (e) => {
 
+    if (e.key === "Enter" || e.keyCode === 13) {
+        let list = document.querySelector("ul");
+        let addTask = document.createElement("li");
+        let textTask = e.target.value;
+        addTask.innerHTML = `<span><i class="fa fa-trash"></i></span>${textTask}`;
+        list.appendChild(addTask)
 
-tareaNueva.addEventListener("keyup", function(e){
-    
-    const list = document.querySelector("ul")
-    
-    if(e.key === "Enter" || e.keyCode === 13){
-        let addTask = document.createElement("li")
-        addTask.appendChild(list)
-        
     }
-    
-    
+    const deleteTask = document.querySelectorAll(".fa-trash");
 
-})
+    deleteTask.forEach(element => {
+        element.addEventListener("click", (e) => {
+            let removeLi = element.parentNode
+            removeLi.parentNode.remove(removeLi);
+        })
+    });
+
+
+
+
+});
+
+// const deleteTask = document.querySelectorAll(".fa-trash");
+
+// deleteTask.forEach(element => {
+//     element.addEventListener("click", (e) => {
+//         let removeLi = element.parentNode
+//         removeLi.parentNode.remove(removeLi);
+//     })
+// });
